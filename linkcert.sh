d7469b749783d78d1f4de1774daa3179b8958f3e
Charles Bushong <cbushong@fearless.tech>
2018-09-25 13:22:33 -0400

    IA-82: Updating defaults, removing linkcert script

--- linkcert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p /etc/letsencrypt/live/$1
-if [ -z "$(ls /etc/letsencrypt/live/$1/)" ]; then
-  echo "Linking files from /etc/letsencrypt/archive/$1/ to /etc/letsencrypt/live/$1/"
-  ln -s /etc/letsencrypt/archive/$1/* /etc/letsencrypt/live/$1/
-else
-  echo "WARN: Files already exist in /etc/letsencrypt/live/$1; cowardly not linking any files"
-fi
